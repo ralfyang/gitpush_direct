5bae369df37602f66c34e68471a37f0cbbf0ef52
Jiook Yang <jiookyang@Jiookui-MacBook-Pro.local>
2016-11-25 09:26:57 +0900

    update for install

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### Made by Ralf Yang
+### goody80762@gmail.com
+
+Exec_dir='/usr/local/bin'
+check_file="/tmp/sudo_check.out"
+
+sudo 2> $check_file
+
+	if [[ `(grep "not found" $check_file)` = "" ]];then
+		SUOD="sudo"
+	else
+		SUDO=""
+	fi
+
+curl -sL http://goody80.github.io/gitpush_direct/gitpush -o /tmp/gitpush
+curl -sL http://goody80.github.io/gitpush_direct/gitpush-branch -o /tmp/gitpush-branch
+
+$SUDO cp -f /tmp/gitpush $Exec_dir
+$SUDO cp -f /tmp/gitpush-branch $Exec_dir
+$SUDO chmod 755 /tmp/gitpush /tmp/gitpush-branch
+rm -f /tmp/gitpush /tmp/gitpush-branch
